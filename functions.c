650f6ae8772d27acead139f603822ffec5857000
Cyrus67 <nickalvarez123@gmail.com>
2018-02-21 16:20:45 -0600

    Did the initial loop
    Yep, that

--- functions.c
+++ functions.c
@@ -15,10 +15,28 @@ int mybarrier(MPI_Comm mcw)
   MPI_Status status;
 
   // Initial loop....
+  for(level = 2, offset = 1;
+    level <= world_size;
+    level = level * 2, offset = offset * 2){
+      if((world_rank % level) == offset){
+        //send
+        MPI_Send(&world_rank, 1, MPI_INT,
+          world_rank - offset, MPI_ANY_TAG, mcw);
+      }
+      else if((world_rank % level) == 0){
+        //receive
+        int dummy;
+        MPI_Recv(&dummy, 1, MPI_INT,
+          world_rank + offset, MPI_ANY_TAG, mcw);
+      }
+      else {
+        continue;
+      }
+    }
 
   tag++;
 
-// Final loop....
+  // Final loop....
 
   return 0;
 }
